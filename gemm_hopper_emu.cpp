357557d01b6940f2ccfe06b63a8f84aa962b5fb2
Ivan Komarov <Ivan.Komarov@dfyz.info>
2026-01-11 16:55:17 +0100

    the Hopper emulation compiles now (but doesn't work yet)

--- gemm_hopper_emu.cpp
+++ gemm_hopper_emu.cpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <array>
+#include <bit>
 #include <limits>
 
 #include <cuda_bf16.h>
@@ -42,6 +43,9 @@ struct Addend {
 }
 
 float MulVecVecHopperEmu(const Vec& vec_a, const Vec& vec_b) {
+    constexpr uint32_t kCarryBits = 5;
+    constexpr uint32_t kExtraSignificandBits = 2;
+
     std::array<Addend, kK> addends;
     for (size_t ii = 0; ii < vec_a.size(); ++ii) {
         const BF16Parts a_parts{vec_a[ii]};
@@ -56,11 +60,11 @@ float MulVecVecHopperEmu(const Vec& vec_a, const Vec& vec_b) {
         );
 
         // Full significands have 8 bits, so the full product will have 16 bits.
-        // The 32-bit addend has 5 most significant bits for intermediate carries,
+        // The 32-bit addend has kCarryBits most significant bits for intermediate carries,
         // so we have to move the MSB of the product (bit 15) to the first free bit.
         const auto addend_significand =
             ((uint32_t)a_parts.full_significand() * (uint32_t)b_parts.full_significand())
-            << (31 - 5 - 15)
+            << (31 - kCarryBits - 15)
         ;
 
         addends[ii] = {
@@ -74,6 +78,46 @@ float MulVecVecHopperEmu(const Vec& vec_a, const Vec& vec_b) {
         return a.exponent > b.exponent;
     });
 
-    // FIXME: return the actual value
-    return 42.0f;
+    Addend result{addends[0]};
+    for (size_t ii = 1; ii < addends.size(); ++ii) {
+        // The below follows the section 7.3 from "Handbook of Floating-Point Arithmetic"
+        //
+        // We know that the exponent of `other` is not greater than the exponent
+        // of the result, so we can immediately align the significand of `other`.
+        auto& other = addends[ii];
+        other.significand >>= std::min(31, result.exponent - other.exponent);
+        if (result.sign == other.significand) {
+            // Perform an addition. This can't make the result negative, and won't overflow,
+            // since we have enough most significant bits to store the carries.
+            result.significand += other.significand;
+        } else {
+            // Perform a subtraction.
+            if (result.significand >= other.significand) {
+                result.significand -= other.significand;
+            } else {
+                // If the result is negative, we should swap the order
+                // of the subtracion and the sign of the result.
+                result.significand = other.significand - result.significand;
+                result.sign ^= 1;
+            }
+        }
+    }
+
+    // `kCarryBits` (plus one additional carry bit for denormalized product) should be zeroes.
+    // Otherwise, we need to re-normalize the result by truncating the significand
+    // and adjusting the exponent.
+    constexpr uint32_t kNeededZeroes = kCarryBits + 1;
+    const auto leading_zeros = std::countl_zero(result.significand);
+    if (kNeededZeroes > leading_zeros) {
+        result.exponent += (kNeededZeroes - leading_zeros);
+        result.significand >>= (kNeededZeroes - leading_zeros);
+    }
+
+    // Compose the final result out of the individual components.
+    return std::bit_cast<float, uint32_t>(
+        (result.sign << 31) |
+        (result.exponent << 23) |
+        // Get rid of the extra bits and the implicit one, if any.
+        ((result.significand >> kExtraSignificandBits) & 0x7'FFFF)
+    );
 }
