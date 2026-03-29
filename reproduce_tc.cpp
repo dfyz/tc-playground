d30ef3924f91ca197cbf8053503c257c53052aae
Ivan Komarov <Ivan.Komarov@dfyz.info>
2026-03-29 00:38:53 +0000

    add threading

--- reproduce_tc.cpp
+++ reproduce_tc.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdio>
 #include <err.h>
+#include <thread>
 #include <vector>
 
 #include <cuda_bf16.h>
@@ -37,31 +38,44 @@ int main() {
     const auto b_mat = ParseFile("b_prefill.bin", kFullN, kFullK);
     const auto c_mat = ParseFile("c_prefill.bin", kFullM, kFullN);
 
-    for (size_t row = 0; row < kFullM; ++row) {
-        printf("%zu/%zu\n", row, kFullM);
-        for (size_t col = 0; col < kFullN; ++col) {
+    std::vector<std::thread> workers;
+    const auto n_workers = std::thread::hardware_concurrency();
+    const auto chunk_size = kFullM / n_workers;
+    for (size_t w_idx = 0; w_idx < n_workers; ++w_idx) {
+        workers.emplace_back([&, w_idx] {
+            const auto start = w_idx * chunk_size;
+            const auto end = (w_idx + 1) * chunk_size;
+            for (size_t row = start; row < end; ++row) {
+                printf("worker %zu: %zu/%zu\n", w_idx, row - start, chunk_size);
+                for (size_t col = 0; col < kFullN; ++col) {
 
-            float acc = 0.0f;
-            for (size_t kk = 0; kk < kFullK; kk += kK) {
-                acc = MulVecVecHopperEmu(
-                    acc,
-                    a_mat[GetPos(row, kk, kFullK) / kK],
-                    b_mat[GetPos(col, kk, kFullK) / kK]
-                );
+                    float acc = 0.0f;
+                    for (size_t kk = 0; kk < kFullK; kk += kK) {
+                        acc = MulVecVecHopperEmu(
+                            acc,
+                            a_mat[GetPos(row, kk, kFullK) / kK],
+                            b_mat[GetPos(col, kk, kFullK) / kK]
+                        );
+                    }
+
+                    const auto our_res = __float2bfloat16(acc);
+                    const auto c_pos = GetPos(row, col, kFullN);
+                    const auto ref_res = c_mat[c_pos / kK][c_pos % kK];
+
+                    if (our_res != ref_res) {
+                        errx(
+                            1,
+                            "difference: %f vs. %f\n",
+                            __bfloat162float(our_res),
+                            __bfloat162float(ref_res)
+                        );
+                    }
+                }
             }
+        });
+    }
 
-            const auto our_res = __float2bfloat16(acc);
-            const auto c_pos = GetPos(row, col, kFullN);
-            const auto ref_res = c_mat[c_pos / kK][c_pos % kK];
-
-            if (our_res != ref_res) {
-                errx(
-                    1,
-                    "difference: %f vs. %f\n",
-                    __bfloat162float(our_res),
-                    __bfloat162float(ref_res)
-                );
-            }
-        }
+    for (auto& ww : workers) {
+        ww.join();
     }
 }
